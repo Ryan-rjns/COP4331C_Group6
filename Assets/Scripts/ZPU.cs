7aa0071bbcbe5cb5c8d8d158aad6c77cbf42d363
Ryan-rjns <rjns@knights.ucf.edu>
2022-02-24 19:17:15 -0500

    Fixed compile error

--- Assets/Scripts/ZPU.cs
+++ Assets/Scripts/ZPU.cs
@@ -7,7 +7,7 @@ public class ZPU : MonoBehaviour{
     public int health;
     public int damage; 
     
-    private GameObject Helicopter = GameOject.FindGameObjectsWithTag("Player");
+    private GameObject Helicopter = GameObject.FindGameObjectsWithTag("Player")[0];
     private bool targetLocked;
     //This is the top of the ZPU turret, it consisted of a ton of objects, so I added them to an array
     public static GameObject[] turretTop = GameObject.FindGameObjectsWithTag("zputop");
