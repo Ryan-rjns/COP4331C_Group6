d8aa1da2b206d6df0093b409924f985bfbd30c7a
Ryan-rjns <rjns@knights.ucf.edu>
2022-03-24 18:49:12 -0400

    Fixed truck rotation

--- Assets/Scripts/Specific Entities/MZKT_6922.cs
+++ Assets/Scripts/Specific Entities/MZKT_6922.cs
@@ -14,7 +14,9 @@ public class MZKT_6922 : MonoBehaviour
     }
 
     private void FixedUpdate(){
+        Vector3 oldRot = transform.rotation.eulerAngles;
         transform.LookAt(target);
-        //transform.rotation *= Quaternion.FromToRotation(Vector3.left, -Vector3.forward);
+        Vector3 newRot = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(new Vector3(oldRot.x, newRot.y, oldRot.z));
     }
 }
