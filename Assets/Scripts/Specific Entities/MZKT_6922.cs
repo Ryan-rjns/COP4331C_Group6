53a98b0b54b1142bd917af4b275647eb4fb460e3
Collin Palm <collinpalm@gmail.com>
2022-03-28 13:36:36 -0400

    truck now tracks model without the nose going up or down

--- Assets/Scripts/Specific Entities/MZKT_6922.cs
+++ Assets/Scripts/Specific Entities/MZKT_6922.cs
@@ -14,9 +14,11 @@ public class MZKT_6922 : MonoBehaviour
     }
 
     private void FixedUpdate(){
-        Vector3 oldRot = transform.rotation.eulerAngles;
-        transform.LookAt(target);
-        Vector3 newRot = transform.rotation.eulerAngles;
-        transform.rotation = Quaternion.Euler(new Vector3(oldRot.x, newRot.y, oldRot.z));
+        
+        Vector3 targetPostition = new Vector3( target.position.x, 
+                                        this.transform.position.y, 
+                                        target.position.z ) ;
+        this.transform.LookAt( targetPostition ) ;
+        
     }
 }
